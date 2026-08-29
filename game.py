d4eee0a0ff2b6bf0054993d163f77584f5d8741c
axelgamerprogrammer <axelgamerprogrammer@gmail.com>
2026-08-29 00:53:28 -0400

    Add api.py and return to default for game.py

--- game.py
+++ game.py
@@ -1,30 +1,28 @@
 import pygame
 import luait
 
-class Game:
-    def __init__(self):
-        self.screen = pygame.display.set_mode((1280, 720))
-        self.clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
 
-        self.script_engine = luait.GameScriptingEngine()
-        self.display_script_engine = luait.GameScriptingDisplayEngine()
+script_engine = luait.GameScriptingEngine()
+display_script_engine = luait.GameScriptingDisplayEngine()
 
-    def run(self):
-        pygame.init()
+display_script_engine.add_binding_function("fill", screen.fill)
 
-        running = True
+running = True
 
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-            self.screen.fill("#7d94b5")
+    screen.fill("#7d94b5")
 
-            self.display_script_engine.update()
+    display_script_engine.update()
 
-            pygame.display.flip()
+    pygame.display.flip()
 
-            self.clock.tick(60)
+    clock.tick(60)
 
-        pygame.quit()
+pygame.quit()
