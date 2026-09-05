45c4c69482919968bdb104d44acee9c040a5127f
axelgamerprogrammer <axelgamerprogrammer@gmail.com>
2026-09-05 00:58:11 -0400

    Complete rework of the execution of modules system

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from lupa.lua55 import LuaRuntime
 import json
 import luait
 import pygame
@@ -38,6 +39,7 @@ class Api:
         self.order_json = json.loads(self.order_file.read_text(encoding="utf-8"))
         self.order: list[str] = self.order_json["order"]
         self.order_path: list[Path] = [Path(path) for path in self.order]
+        self.modules: list[luait.Module] = []
 
         self.start_module: list = []
         self.update_module: list = []
@@ -45,42 +47,30 @@ class Api:
 
     def init(self):
         self.script_engine.init()
-        self.register_api()
 
-        #self.script_engine.lua.globals().print = None
-        self.script_engine.lua.globals().require = None
-        self.script_engine.lua.globals().dofile = None
-        self.script_engine.lua.globals().loadfile = None
+        for module_path in self.order_path:
+            self.modules.append(luait.Module(module_path, self.register_api))
 
-        for module in self.order_path:
-            env = self.script_engine.lua.table()
-            env._name = module.name
+        for module in self.modules:
+            self.script_engine.logger.info(f"{module.module_name} loaded")
+            module.remove_functions(["require", "dofile", "loadfile", "print"])
+            module.execute()
 
-            self.script_engine.logger.info(f"{module.name} loaded")
-            self.script_engine.execute_file(module, module.name)
-
-            self.start_module.append(self.script_engine.lua.globals().start)
-            self.update_module.append(self.script_engine.lua.globals().update)
-            self.render_module.append(self.script_engine.lua.globals().render)
-
-    def register_api(self):
-        self.script_engine.expose_api(BaseApi())
-        #self.script_engine.expose_api(ConsoleApi(self.script_engine))
+    def register_api(self, lua:LuaRuntime, module_name:str):
+        self.script_engine.expose_api(BaseApi(), lua)
+        self.script_engine.expose_api(ConsoleApi(module_name), lua)
 
     def start_event(self):
-        for start in self.start_module:
-            if start is not None:
-                start()
+        for module in self.modules:
+            module.start()
 
     def update_event(self):
-        for update in self.update_module:
-            if update is not None:
-                update()
+        for module in self.modules:
+            module.update()
 
     def render_event(self):
-        for render in self.render_module:
-            if render is not None:
-                render()
+        for module in self.modules:
+            module.render()
 
 # Modding Api
 
@@ -88,13 +78,18 @@ class BaseApi(ModdingApi):
     def __init__(self):
         super().__init__(False)
 
-"""
-class ConsoleApi(ModdingApi):
-    def __init__(self, script_engine:luait.GameScriptingEngine):
-        super().__init__(True, "console")
-        self.script_engine = script_engine
 
-    def api_log(self, message):
-        logger_log = logger.bind(thread="lua", source="")
-        logger_log.info(message)
-"""
+class ConsoleApi(ModdingApi):
+    def __init__(self, module_name:str):
+        super().__init__(True, "console")
+        self.module_name = module_name
+        self.console_log = logger.bind(thread="lua", source=self.module_name)
+
+    def api_info(self, message):
+        self.console_log.info(message)
+
+    def api_warn(self, message):
+        self.console_log.warning(message)
+
+    def api_err(self, message):
+        self.console_log.error(message)
--- luait.py
+++ luait.py
@@ -1,12 +1,53 @@
 from lupa.lua55 import LuaRuntime
 from pathlib import Path
 from loguru import logger
+from typing import Callable
 import json
 import api
 
+class Module:
+    def __init__(self, path:Path, api_function:Callable):
+        self.path: Path = path
+        self.module_name: str = path.name
+        self.lua: LuaRuntime = LuaRuntime()
+
+        api_function(self.lua, self.module_name)
+
+        self.start_function: Callable = None
+        self.render_function: Callable = None
+        self.update_function: Callable = None
+
+        env = self.lua.table()
+        env.module_name = self.module_name
+
+        self.lua.globals()._env = env
+
+    def execute(self):
+        self.lua.execute(self.path.read_text(encoding="utf-8"))
+
+        self.start_function = self.lua.globals().start
+        self.render_function = self.lua.globals().render
+        self.update_function = self.lua.globals().update
+
+    def remove_functions(self, functions_list:list):
+        for function in functions_list:
+            self.lua.globals()[function] = None
+
+    def start(self):
+        if self.start_function is not None:
+            self.start_function()
+
+    def render(self):
+        if self.render_function is not None:
+            self.render_function()
+
+    def update(self):
+        if self.update_function is not None:
+            self.update_function()
+
+
 class GameScriptingEngine:
     def __init__(self):
-        self.lua = LuaRuntime()
         self.logger = logger.bind(thread="lua", source="GameScriptingEngine")
 
         self.modules_order_file: Path = None
@@ -23,22 +64,16 @@ class GameScriptingEngine:
             default_json = {"order": []}
             self.modules_order_file.write_text(json.dumps(default_json, indent=2))
 
-    def execute(self, code:str):
-        self.lua.execute(code)
-
-    def execute_file(self, path:Path, name):
-        self.lua.execute(f"_name = '{name}'\n{path.read_text(encoding="utf-8")}")
-
-    def expose_api(self, modding_api:api.ModdingApi):
+    def expose_api(self, modding_api:api.ModdingApi, lua:LuaRuntime):
         if modding_api.is_table:
-            table = self.lua.table()
+            table = lua.table()
             for function_name in modding_api.get_api_functions():
                 table[function_name.replace("api_", "")] = getattr(modding_api, function_name)
 
-            setattr(self.lua.globals(), modding_api.name, table)
+            setattr(lua.globals(), modding_api.name, table)
         else:
             for function_name in modding_api.get_api_functions():
-                setattr(self.lua.globals(), function_name.replace("api_", ""), getattr(modding_api, function_name))
+                setattr(lua.globals(), function_name.replace("api_", ""), getattr(modding_api, function_name))
 
 class DisplayFunction:
     def __init__(self, name:str, args:list):
